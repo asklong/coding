d7a63dd10bb74db699ef48bfff46ff8398d3fa62
asklong <499671517@qq.com>
2017-01-20 22:54:35 -0600

    new

---
+++ src/DFS/Graph.java
@@ -0,0 +1,8 @@
+package DFS;
+
+public class Graph {
+	//topological sorting for directed acyclic graph is a
+	//linear ordering of vertices such that for every directed edge uv,
+	// vertex u comes before v in the ordering.
+	
+}
